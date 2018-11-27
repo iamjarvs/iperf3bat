67d2db85f44099b5a6ec5e16fb264bda9276c73c
Adam Jarvis <30406673+iamjarvs@users.noreply.github.com>
2018-11-27 11:48:56 +0000

    Update iperfClient.bat

--- iperfClient.bat
+++ iperfClient.bat
@@ -3,10 +3,6 @@
 @REM Connects to the iperf-server host and begins data transfer.
 @REM Updated 2008-02-13
 
-IF NOT EXIST Lib\iperf.exe ECHO could NOT find iperf.exe. Unable to continue & exit 1
-IF NOT EXIST Lib\grep.exe ECHO could NOT find grep.exe. A CSV log will not be generated.
-IF NOT EXIST Lib\sed.exe ECHO could NOT find sed.exe. A CSV log will not be generated.
-
 :SETUP
 SET SERVER="iperf.hyperoptic.com"
 SET PORT=5210
@@ -31,33 +27,12 @@ SET SEDCMD="s/sec//g; s/MBytes//g; s#Mbits/##g; s/-[ ]/-/g; s/[ ]\+/,/g; s/$/\r/
 REM SET /A TESTLEN *= 60
 
 ECHO Test started on %DATE% %TIME%
-start Lib\tail.exe -F "%LOGFILE%.log"
-IF %UDP% == n Lib\iperf.exe -c %SERVER% -p %PORT% -d -w %TCPWIN% -f %FORMAT% -i %INTERVAL% -t %TESTLEN% >> "%LOGFILE%.log"
-IF %UDP% == N Lib\iperf.exe -c %SERVER% -p %PORT% -d -w %TCPWIN% -f %FORMAT% -i %INTERVAL% -t %TESTLEN% >> "%LOGFILE%.log"
-IF %UDP% == y Lib\iperf.exe -c %SERVER% -p %PORT% -d -u -w %TCPWIN% -f %FORMAT% -i %INTERVAL% -t %TESTLEN%  >> "%LOGFILE%.log"
-IF %UDP% == Y Lib\iperf.exe -c %SERVER% -p %PORT% -d -u -w %TCPWIN% -f %FORMAT% -i %INTERVAL% -t %TESTLEN%  >> "%LOGFILE%.log"
 
-IF EXIST Lib\grep.exe IF EXIST Lib\sed.exe GOTO PARSELOG
+IF %UDP% == n iperf.exe -c %SERVER% -p %PORT% -d -w %TCPWIN% -f %FORMAT% -i %INTERVAL% -t %TESTLEN% >> "%LOGFILE%.log"
+IF %UDP% == N iperf.exe -c %SERVER% -p %PORT% -d -w %TCPWIN% -f %FORMAT% -i %INTERVAL% -t %TESTLEN% >> "%LOGFILE%.log"
+IF %UDP% == y iperf.exe -c %SERVER% -p %PORT% -d -u -w %TCPWIN% -f %FORMAT% -i %INTERVAL% -t %TESTLEN%  >> "%LOGFILE%.log"
+IF %UDP% == Y iperf.exe -c %SERVER% -p %PORT% -d -u -w %TCPWIN% -f %FORMAT% -i %INTERVAL% -t %TESTLEN%  >> "%LOGFILE%.log"
 
 :END
 ECHO Done
 PAUSE
-EXIT 0
-
-:PARSELOG
-Lib\grep.exe %GREPCMD% "%LOGFILE%.log" >> "%LOGFILE%.tmp"
-ECHO [ID],Interval (s),Transfer (MBytes),Bandwidth (Mbits/sec) > "%LOGFILE%.csv"
-Lib\sed.exe %SEDCMD% "%LOGFILE%.tmp" >> "%LOGFILE%.CSV"
-del "%LOGFILE%.tmp"
-GOTO END
-
-:OVERWRITELOG
-SET ANS=y
-@REM  ECHO ** "%LOGFILE%.log" already exists.  Would you like to overwrite this file?
-@REM  SET /P ANS=   (y/N): 
-IF "%ANS%"=="n" GOTO SETUP
-IF "%ANS%"=="N" GOTO SETUP
-IF "%ANS%"=="y" del "%LOGFILE%.log" & GOTO CONTINUE
-IF "%ANS%"=="Y" del "%LOGFILE%.log" & GOTO CONTINUE
-ECHO    "%ANS%" is not valid
-GOTO END
