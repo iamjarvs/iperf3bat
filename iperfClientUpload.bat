fc418951d7b3a56f2fe0b29c80f8a63d12433709
Adam Jarvis <30406673+iamjarvs@users.noreply.github.com>
2018-11-27 12:15:10 +0000

    Update iperfClientUpload.bat

--- iperfClientUpload.bat
+++ iperfClientUpload.bat
@@ -3,6 +3,7 @@
 @REM Connects to the iperf-server host and begins data transfer.
 @REM Credit matthewlinton - original repo - https://github.com/matthewlinton/Windows-Scripts/blob/master/iPerf/iperf-client.bat
 @REM Updated 27-11-2018
+@REM the path for the iperf3.exe needs to be 'C:\iperf-3\iperf3.exe' or change path in line 22
 
 :SETUP
 SET SERVER="iperf.hyperoptic.com"
@@ -16,23 +17,9 @@ SET LOGFILE=C:\logfile
 SET GREPCMD="Mbits/sec$"
 SET SEDCMD="s/sec//g; s/MBytes//g; s#Mbits/##g; s/-[ ]/-/g; s/[ ]\+/,/g; s/$/\r/g"
 
-@REM  SET /P LOGFILE=Enter in a logfile name (w/o extension): 
-@REM IF EXIST "%LOGFILE%.log" GOTO OVERWRITELOG
-@REM :CONTINUE
-@REM  SET /P SERVER=Enter in the iPerf server's IP address: 
-@REM SET /P PORT=Enter a port to run on (%PORT%): 
-@REM  SET /P UDP=Run UDP test (y/N): 
-@REM  SET /P TESTLEN=Enter test duration in minutes (%TESTLEN%): 
-@REM  SET /P INTERVAL=Enter the output interval in seconds (%INTERVAL%): 
-
-REM SET /A TESTLEN *= 60
-
 ECHO Test started on %DATE% %TIME%
 
 IF %UDP% == n C:\iperf-3\iperf3.exe -c %SERVER% -p %PORT% -d -w %TCPWIN% -f %FORMAT% -i %INTERVAL% -t %TESTLEN%
-IF %UDP% == N C:\iperf-3\iperf3.exe -c %SERVER% -p %PORT% -d -w %TCPWIN% -f %FORMAT% -i %INTERVAL% -t %TESTLEN%
-IF %UDP% == y C:\iperf-3\iperf3.exe -c %SERVER% -p %PORT% -d -u -w %TCPWIN% -f %FORMAT% -i %INTERVAL% -t %TESTLEN%
-IF %UDP% == Y C:\iperf-3\iperf3.exe -c %SERVER% -p %PORT% -d -u -w %TCPWIN% -f %FORMAT% -i %INTERVAL% -t %TESTLEN%
 
 :END
 ECHO Done
